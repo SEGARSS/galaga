d0a53c8bce58fb7486ed6802ac425fe854fbfe87
SEGARSS <vim_branet@mail.ru>
2025-07-07 09:51:24 +0500

    дз

--- galaga/main.cpp
+++ galaga/main.cpp
@@ -145,23 +145,24 @@ int main()
                 pos.y -= 50.f; //Назначаем ей направление, куда ей лететь.
                 bulletsPlayer[i].setPosition(pos); // И уже ставим эту позию для пули.
 
-                //Vector2f posEnemy = enemy.getPosition();
-                //if (pos.y == posEnemy.y)
-                //{
-                //    cout << "Hit! \n";
-                //}
+                Vector2f posEnemy = enemy.getPosition();
+                if (pos.y == posEnemy.y || pos.x <= posEnemy.x)
+                {
+                    cout << "Hit! \n";
+                }
+                else if (pos.y == posEnemy.y || pos.x != posEnemy.x);
             }            
         }
 
-        for (int i = 0; i < bulletsPlayer.size(); i++)
-        {
-            Vector2f posBullets = bulletsPlayer[i].getPosition();
-            Vector2f posEnemy = enemy.getPosition();
-            if (posBullets.y == posEnemy.y)
-            {
-                cout << "Hit! \n";
-            }
-        }
+        //for (int i = 0; i < bulletsPlayer.size(); i++)
+        //{
+        //    Vector2f posBullets = bulletsPlayer[i].getPosition();
+        //    Vector2f posEnemy = enemy.getPosition();
+        //    if (posBullets.y == posEnemy.y)
+        //    {
+        //        cout << "Hit! \n";
+        //    }
+        //}
 
         // Clear screen
         window.clear();
